419b439482bf817f980425fe82bdd80c71065f7b
avr-ethersex <avr@sylph.be>
2013-08-01 10:29:56 +0200

    Create main.py

---
+++ RaspberryPI/i2c/datacollector/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import MySQL
+import ethersex as ecmd
+from time import sleep
+
+hostname=ecmd.get_hostname()
+version=ecmd.get_version()
+
+print("Data collector i2c\n")
+print("------------------\n")
+print(hostname,version)
+
+
+while True :
+        sensor_id,temperature=ecmd.get_temperature()
+        MySQL.insert_temperature(sensor_id,temperature)
